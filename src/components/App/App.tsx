83b22918e07112b1d75a7ecbb10222bccac9c6fe
Maksym Skubiak <skubiakcreates@gmail.com>
2026-07-23 19:53:15 +0200

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import css from "./App.module.css";
 import { useState } from "react";
-import CafeInfo from "./CafeInfo/CafeInfo";
-import VoteOptions from "./VoteOptions/VoteOptions";
-import VoteStats from "./VoteStats/VoteStats";
-import Notification from "./Notification/Notification";
-import type { Votes, VoteType } from "../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+import type { Votes, VoteType } from "../../types/votes";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
